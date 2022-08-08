5028c3f50ea5df041ad82700476e3f108feb57a6
Jesper Kuusela <103361843+Ballom123@users.noreply.github.com>
2022-08-08 19:45:27 +0300

    Update News API program.py
    Pull request is 400 when keywords are left empty. No idea how to fix.

--- News API program.py
+++ News API program.py
@@ -3,45 +3,44 @@ import datetime
 import json
 
 class Newsroom:
-    def __init__(self, keyword: str, date: datetime, lang: str, newsbase:int, sortby: int ):
+    def __init__(self, newsbase:str, keyword: str, sortby: str ):
         
-        
-        self.keywords = "q=" + keyword
-        self.language = "language=" + lang
-        self.date = "to=" + date.strftime("%Y-%m-%d")
-        #Get your own API key from newsapi.org!
+        self.keywords = self.make_query_string(keyword)
+        #create todays date to string w/ datetime lib
+        self._date = "to=" + (datetime.date.today()).strftime("%Y-%m-%d")
+        #get your own API key from newapi.org!!!!!
+        #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         self._apikey = "apiKey="
-
-        if newsbase == 1:
-            self.newsbase = "/v2/everything?"
-        elif newsbase == 2:    
-            self.newsbase= "/v2/top-headlines?"
         
-        if sortby == 1:
-            self.sortby= "sortBy=relevancy"
-        elif sortby == 2:
-            self.sortby= "sortBy=popularity"
-        elif sortby == 3:
-            self.sortby= "sortBy=publishedAt"
+        self.newsbase = newsbase
+        
+        self.sortby = sortby
 
         #build the url for the requested queries
         self.compile_url()
         #connect to api with url
         self.connection_to_api()
+        #do stuff with results
+        self.playground()
 
     def compile_url(self):
         #test url, "iphone", "english", "by popularity"
-        #self.news_url = "https://newsapi.org/v2/everything?q=apple&language=en&from=2022-07-16&to=2022-07-16&sortBy=popularity&apiKey="
-        self.news_url = f"https://newsapi.org{self.newsbase}{self.keywords}&{self.language}&{self.date}&{self.sortby}&{self._apikey}"
+        #self._news_url = "https://newsapi.org/v2/everything?q=apple&language=en&from=2022-07-16&to=2022-07-16&sortBy=popularity&apiKey="
+        self._news_url = f"https://newsapi.org{self.newsbase}{self.keywords}&language=en&{self._date}&{self.sortby}&{self._apikey}"
 
     def connection_to_api(self):
         #connect to API
-        self.news_request = requests.get(self.news_url)
-        #json magics happen
-        self.response = self.news_request.json()
-        self.articles = self.response["articles"]
-        #ask for how many to see
+        self.news_request = requests.get(self._news_url)
+        
+        
+    def playground(self):
+        #json magics
+        self.articles = self.news_request.json()['articles']
+        self.authors = {article['author'] for article in self.articles}
+        self.sources = set([article['source']['id'] for article in self.articles])
         print(f"Found {len(self.articles)} articles")
+        print(f"{len(self.authors)} unique authors and {len(self.sources)} unique sources")
+        
         results = int(input("How many do you want to see: "))
         if results > len(self.articles) or results<0:
             raise ValueError("Number out of range!")
@@ -55,29 +54,61 @@ class Newsroom:
 
 
 
+    def make_query_string(self, keyword):
+        if keyword == "":
+            return ""
+        return "q=" + keyword
+
+
+
+
+#Methods for functional programming
+#Asks for searchterms and do error checks
+def check_keyword():
+    print(f"Only whole dataset supports multiple keyword searches!!!\nLeave field blank if anything goes.")
+    search_words = input(f"Use \"\" for exact matching, + if the term must appear: ")
+    if len(search_words)>500:
+        raise ValueError("Search parameter too long! 500 characters or less!")
+    if len(search_words) == 0:
+        return ""
+    
+    phrase_list = search_words.split(" ")
+    search_words = ("+").join(phrase_list)
+    return search_words
+
+#Ask for which database to search and do error checks.
+def check_dataset():
+    try:
+        data_value = int(input("Search whole dataset (1) or top headlines only (2): "))
+    except:
+        raise ValueError("Only values 1 or 2 qualify!")
+    if data_value != 1 and data_value != 2:
+        raise ValueError("Only values 1 or 2 qualify!")
+
+
+    return ["/v2/everything?", "/v2/top-headlines?"][data_value-1]
+
+    
+
+#which way to sort
+def check_sortby():
+    try:
+        sort_value = int(input("Sort by relevancy (1), popularity (2), new (3): "))
+    
+    #error in not number
+    except:
+        raise ValueError("Only values 1-3 qualify!")
+    #error wrong numbers
+    if 1> sort_value >3:
+        raise ValueError("Only values 1-3 qualify!")
+
+    sort_list=["sortBy=relevancy", "sortBy=popularity", "sortBy=publishedAt"]
+    return sort_list[sort_value-1] 
+    
 
 if __name__ == "__main__":
     #ask for some variables for what news you want to see
-    #keyword, date, language, source domain
-    print(f"Hello, what kind of news are you looking for?")
-    print(f"Leave field blank if anything goes")
-    news_words = input(f"Keywords in article\nUse \"\" for exact match, + if must appear: ")
-    news_date = datetime.date.today()
-    news_lang = input(f"Choose a language\nar, de, en, es, fr, he, it, nl, no, pt, ru, sv, ud, zh: ")
-    if news_lang not in ["ar", "de", "en", "es", "fr", "he", "it", "nl", "no", "pt", "ru", "sv", "ud", "zh"]:
-        raise ValueError("Language not from list!")
-    news_top_or_not = int(input("Whole dataset (1) or top headlines (2): "))
-    if news_top_or_not != 1 and news_top_or_not != 2:
-        raise ValueError("Only values 1 or 2 qualify!")
-    news_sort = int(input("Sort by relevancy (1), popularity (2), new (3): "))
-    if 1> news_sort >3:
-        raise ValueError("Only values 1-3 qualify!")
+    print(f"Hello, what kind of news are you looking for today?")
 
-    #Test variables
-    #news_words = "iphone"
-    #news_date = ""
-    #news_lang = "english"
-    #news_top_or_not = 2
-    
     #actual API processing
-    Newsroom(news_words, news_date, news_lang, news_top_or_not, news_sort)
+    Newsroom(check_dataset(), check_keyword(), check_sortby())
